28bc84b290b2924e50d571101702a21b7ebe52aa
Danny Ocean <danny.danh.nguyen@gmail.com>
2018-07-10 09:07:11 -0700

    added auto render and button call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,14 @@
-// Submit Click Function To Insert City into API search
-$("#add-city").on("click", function (event) {
+/////// HERE IS WHERE APIS WILL BE CALLED IMMEDIATELY UPON CITY INPUT SUBMIT
+
+// Submit Click Function To Insert City into API search (Automatic Render)
+$("#add-city").on("click", function(event) {
     event.preventDefault();
 
     // Clears Previous Search
-    $("#city-input").val("");
+    $("#auto-view").empty();
+    
+    //////////////////// Add API functions here (APIS WE WANT TO RUN INSTANTLY)
+    console.log("HELLO, THIS IS A TEST");
 
     // Weather API function
     function weather() {
@@ -22,48 +27,71 @@ $("#add-city").on("click", function (event) {
             let weatherResults = response;
         })
     }
-    // EventBrite API
-    function eventBriteInfo() {
-
-        // Use location var with $("#city-input") submission
-        var location = $("#city-input").val().trim();
-        var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=SZDJEP44NSPTC2RGIFBQ&location.address=" + location;
-    
-        // AJAX Call
-        $.ajax({
-            url: queryURL,
-            method: "GET"
-        }).then(function (response) {
-
-            var results = response;
-            console.log(results);
-            console.log(results.events);
-            console.log(results.events[0]);
-            console.log(results.events[0].logo.url);
-
-            var header = $("<h1>")
-            header.text("Local Events");
-
-            for (var j = 0; j < 3; j++) {
-                var eventDiv = $("<div>");
-
-                var imgURL = results.events[j].logo.url;
-                var description = results.events[j].description.text
-
-                var image = $("<img>");
-                image.attr("src", imgURL);
-
-                var details = $("<p>");
-                details.text(description)
-
-                eventDiv.append(header);
-                eventDiv.append(image);
-                eventDiv.append(details);
-                $("#events-view").prepend(eventDiv);
-            }
-        });
-    }
+    /////////////////////////////////////////
 
     //API Initializers (Call API functions below so that they will run on submit)
+    renderButtons();
     eventBriteInfo();
+    $("#events-view").hide();
+});
+
+/////// BELOW ARE APIS TO BE CALLED ONLY ON BUTTON CLICK
+
+// EventBrite API
+function eventBriteInfo() {
+
+    // Use location var with $("#city-input") submission
+    var location = $("#city-input").val().trim();
+    var queryURL = "https://www.eventbriteapi.com/v3/events/search/?token=SZDJEP44NSPTC2RGIFBQ&location.address=" + location;
+
+    // AJAX Call
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response) {
+    
+        var results = response;
+        console.log(results);
+        console.log(results.events);
+        console.log(results.events[0]);
+        console.log(results.events[0].logo.url);
+
+        var header = $("<h1>")
+            header.text("Local Events");
+            
+        for (var j = 0; j < 3; j++) {
+            var eventDiv = $("<div>");
+    
+            var imgURL = results.events[j].logo.url;
+            var description = results.events[j].description.text
+
+            var image = $("<img>");
+            image.attr("src", imgURL);
+
+            var details = $("<p>");
+            details.text(description)
+    
+            eventDiv.append(header);
+            eventDiv.append(image);
+            eventDiv.append(details);
+            $("#events-view").prepend(eventDiv);
+        }
+    });
+}
+
+// Function to Render Button Features after City Input is Submitted
+function renderButtons() {
+  
+    $("#buttons-view").empty();
+  
+    var eventButton = $("<button>");
+    eventButton.addClass("btn btn-info border");
+    eventButton.attr("id", "eventBrite");
+    eventButton.text("Events");
+    $("#buttons-view").append(eventButton);
+}
+
+// EventBrite On Click Function
+$(document).on("click", "#eventBrite", function(event) {
+    $("#events-view").show();
 });
